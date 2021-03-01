e0a88d3da21af40ccebee4c0e406290c1341056e
Emiliano Arregui <emiliano.arregui@gmail.com>
2021-03-01 16:27:05 -0300

    eliminación de antiscrapping

--- src/routes/dni.js
+++ src/routes/dni.js
@@ -3,70 +3,49 @@ const router = express.Router();
 const Afiliadx = require('../models/Afiliadx');
 const isAuthenticated = require('../passport/local-auth');
 
-function numRandom(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
 
 router.get('/dni', isAuthenticated,(req,res) => {
 
-    sum1 = numRandom(1,11)
-    sum2 = numRandom(1,11)
-    resultado = sum1 + sum2
     afiliadx = {
         texto: '',
         display: 'invisible',
         color: ''
     }
-    res.render('dni',{
-        resultado,
-        afiliadx,
-        sum1,
-        sum2
-    });
+    res.render('dni',{afiliadx});
 });
 
 router.post('/dni', isAuthenticated, (req,res)=> {
-    const {dni, suma} = req.body;
+    const dni = req.body.dni;
 
-    res.redirect(`/dni/${dni}/${suma}`);
+    res.redirect(`/dni/${dni}`);
 })
 
-router.get('/dni/:_dni/:_suma', isAuthenticated, async (req,res) => {
+router.get('/dni/:_dni', isAuthenticated, async (req,res) => {
 
-    const { _dni, _suma } = req.params;
+    const _dni = req.params._dni;
     afiliadx = {
         texto : '',
         display: 'visible',
         color: ''
     }
 
-    if (resultado === parseInt(_suma)) {
-        const consulta = await Afiliadx.findOne({dni: _dni});
-        if (consulta) {
-            afiliadx._id = consulta._id
-            if (consulta.confirmada === true) {
-                afiliadx.texto = `${consulta.nombre} ${consulta.apellido} DNI: ${consulta.dni} está afiliadx! :)`;            
-                afiliadx.color = 'success'
-            } else {
-                afiliadx.texto = `La afiliación de ${consulta.nombre} ${consulta.apellido} DNI: ${consulta.dni} fue enviada pero rebotó o se cayó :/`;
-                afiliadx.color = 'warning'
-            }
+    const consulta = await Afiliadx.findOne({dni: _dni});
+
+    if (consulta) {
+        afiliadx._id = consulta._id
+        if (consulta.confirmada === true) {
+            afiliadx.texto = `${consulta.nombre} ${consulta.apellido} DNI: ${consulta.dni} está afiliadx! :)`;            
+            afiliadx.color = 'success'
         } else {
-            afiliadx.texto = 'No hay afiliadxs con ese DNI';
+            afiliadx.texto = `La afiliación de ${consulta.nombre} ${consulta.apellido} DNI: ${consulta.dni} fue enviada pero rebotó o se cayó :/`;
             afiliadx.color = 'warning'
         }
     } else {
-        afiliadx.texto = 'Sacaste mal la cuenta';
-        afiliadx.color = 'danger'
+        afiliadx.texto = 'No hay afiliadxs con ese DNI';
+        afiliadx.color = 'warning'
     }
-    var sum1 = numRandom(1,11)
-    var sum2 = numRandom(1,11)
-    resultado = sum1 + sum2
-    res.render('dni',{
-        afiliadx,
-        sum1,
-        sum2
-    });
+
+    res.render('dni',{afiliadx});
 });
 
 module.exports = router;
--- src/routes/nombre.js
+++ src/routes/nombre.js
@@ -4,15 +4,9 @@ const router = express.Router();
 const Afiliadx = require('../models/Afiliadx');
 const isAuthenticated = require('../passport/local-auth');
 
-function numRandom(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
 
 router.get('/nombre', isAuthenticated, (req,res) => {
 
-    sum1 = numRandom(1,11)
-    sum2 = numRandom(1,11)
-    resultado = sum1 + sum2
     afiliadx = {
         texto: '',
         display: 'invisible',
@@ -21,83 +15,68 @@ router.get('/nombre', isAuthenticated, (req,res) => {
     consulta = {display : 'invisible'}
     res.render('nombre',{
         consulta,
-        resultado,
-        afiliadx,
-        sum1,
-        sum2
+        afiliadx
     });
 });
 
 router.post('/nombre', isAuthenticated, (req, res) => {
-    const { nombre, apellido, suma} = req.body;
+    const { nombre, apellido} = req.body;
     if (nombre === '') {
-        res.redirect(`nombre/${suma}/${apellido}/*`)
+        res.redirect(`nombre/${apellido}/*`)
     }
-    res.redirect(`nombre/${suma}/${apellido}/${nombre}`)
+    res.redirect(`nombre/${apellido}/${nombre}`)
 
 })
 
-router.get('/nombre/:_suma/:_apellido/:_nombre', isAuthenticated, async (req, res) => {
-    const { _nombre, _apellido, _suma } = req.params;
+router.get('/nombre/:_apellido/:_nombre', isAuthenticated, async (req, res) => {
+    const { _nombre, _apellido } = req.params;
     afiliadx = {
         texto: '',
         color: '',
         display: 'visible'
     }
-    if (!(resultado === parseInt(_suma))) {
-        consulta = {display:'invisible'};
-        afiliadx = {
-            texto :'Sacaste mal la cuenta',
-            color :'danger',
-            display :'visible'
+
+    var consulta = await Afiliadx.find({
+        nombre: {
+            $regex: `${_nombre}`,
+            $options: 'si'
+        },
+        apellido:{
+            $regex: `${_apellido}`,
+            $options: 'si'
         }
+    })
+    if (consulta.length > 0) {
+
+        consulta.display = 'visible';
+        afiliadx.display = 'invisible'
+
     } else {
+
         var consulta = await Afiliadx.find({
-            nombre: {
-                $regex: `${_nombre}`,
-                $options: 'si'
-            },
             apellido:{
                 $regex: `${_apellido}`,
                 $options: 'si'
             }
         })
+
         if (consulta.length > 0) {
 
             consulta.display = 'visible';
-            afiliadx.display = 'invisible'
-
+            afiliadx.display = 'invisible';
         } else {
-
-            var consulta = await Afiliadx.find({
-                apellido:{
-                    $regex: `${_apellido}`,
-                    $options: 'si'
-                }
-            })
-
-            if (consulta.length > 0) {
-
-                consulta.display = 'visible';
-                afiliadx.display = 'invisible';
-            } else {
-                consulta.display = 'invisible';
-                afiliadx = {
-                    display: 'visible',
-                    texto: 'No hay afiliadxs con ese nombre',
-                    color: 'warning'
-                }
+            consulta.display = 'invisible';
+            afiliadx = {
+                display: 'visible',
+                texto: 'No hay afiliadxs con ese nombre',
+                color: 'warning'
             }
         }
     }
 
-    sum1 = numRandom(1,11)
-    sum2 = numRandom(1,11)
-    resultado = sum1 + sum2
 
     res.render('nombre',{
         consulta,
-        resultado,
         afiliadx
     })
 })
