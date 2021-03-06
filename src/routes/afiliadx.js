3182ce56eec99a0495782f7ab1f34c593888baee
Emiliano Arregui <emiliano.arregui@gmail.com>
2021-03-06 00:42:23 -0300

    correcciones menores

--- src/routes/afiliadx.js
+++ src/routes/afiliadx.js
@@ -45,7 +45,7 @@ router.get('/afiliadx/editar/:id', isAuthenticated,async (req, res) => {
 router.post('/afiliadx/editar/:id', isAuthenticated, async (req, res) => {
     var afiliadx = await Afiliadx.findByIdAndUpdate(req.params.id, req.body)
     afiliadx.save()
-    res.redirect(back)
+    res.redirect(`${back}#${id}`)
 })
 
 module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isAuthenticated = require('../passport/local-auth')
 const Afiliadx = require('../models/Afiliadx')
+const device = require('device')
 
 router.get('/', isAuthenticated, (req, res) => {
     var lista = false
@@ -10,10 +11,10 @@ router.get('/', isAuthenticated, (req, res) => {
 } )
 
 router.get('/stats', async (req,res) => {
-    var lista = await Afiliadx.find({'estado.votante': true, 'estado.voto':false, 'estado.voto':null}).sort({contacto:'desc', apellido:'asc'})
+    var myDevice = device(req.headers['user-agent']).type
+    var lista = await Afiliadx.find({'estado.votante': true}).sort({contacto:'desc', apellido:'asc'})
     var query = await Afiliadx.distinct('dni',{confirmada: true});
     var total = query.length
-
     var numeros = {
         total: total,
         minimo: Math.round(total * 0.1),
@@ -26,7 +27,7 @@ router.get('/stats', async (req,res) => {
     numeros.por_voto = Math.round((numeros.voto * 100) / numeros.minimo);
     numeros.por_votante = Math.round((numeros.votante * 100) / numeros.minimo);
  
-    res.render('index',{numeros, lista})
+    res.render('index',{numeros, lista, myDevice})
 })
 
 
